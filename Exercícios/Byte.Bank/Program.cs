b44412634725996404a8aae19a7a8bfdb75dfa8e
Renan Salvino <renansalvinomartins454@gmail.com>
2019-10-03 17:23:10 -0300

    03/10

---
+++ Exercícios/Byte.Bank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Byte.Bank {
+    class Program {
+        static void Main (string[] args) {
+
+            
+            System.Console.WriteLine("ByteBank - Cadastro do cliente");
+            System.Console.WriteLine ("Nome: ");
+            string nome = Console.ReadLine ();
+            System.Console.WriteLine ("CPF: ");
+            string CPF = Console.ReadLine ();
+            System.Console.WriteLine ("E-mail: ");
+            string email = Console.ReadLine ();
+        
+
+            Cliente cliente1 = new Cliente (nome, CPF, email);
+
+            bool trocouSenha = false;
+            do{
+                Console.Write("Senha: ");
+                string senha = Console.ReadLine();
+                trocouSenha = cliente1.TrocaSenha(senha);
+                if(trocouSenha){
+                    System.Console.WriteLine("Senha alterada com sucesso!");
+                }else{
+                    System.Console.WriteLine("senha inválida");
+                }
+            }while(!trocouSenha);
+            Console.Clear();
+            
+        }
+    }
+}
---
+++ Exercícios/Byte.Bank/cliente.cs
@@ -0,0 +1,25 @@
+namespace Byte.Bank {
+    public class Cliente 
+    {
+        public string Nome;
+        public string Senha;
+        public string CPF;
+        public string Email;
+        public Cliente (string Nome, string CPF, string Email){
+
+        this.Nome = Nome;
+        this.CPF = CPF;
+        this.Email = Email;
+    }
+
+        public bool TrocaSenha (string Senha){
+            if ((Senha.Length > 6) && (Senha.Length <16)){
+                this.Senha = Senha;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+    }
+}
