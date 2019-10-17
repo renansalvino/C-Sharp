c24d5d13a5a800242c983d2251baab11754f5c1e
Renan Salvino <renansalvinomartins454@gmail.com>
2019-10-17 17:17:41 -0300

    17/10

--- Exercícios erro/EscolaDeRock/Program.cs
+++ Exercícios erro/EscolaDeRock/Program.cs
@@ -9,7 +9,7 @@ namespace EscolaDeRock
         QUARTETO
     };
 
-    enum InstrumentoMusical : int
+    enum InstrumentoEnum : int
     {
         BAIXO,
         BATERIA,
@@ -20,23 +20,81 @@ namespace EscolaDeRock
         VIOLÃO
     };
 
-    enum CategoriaEnum: int 
+    enum CategoriaEnum : int
     {
-        RITMO,
         HARMONIA,
         MELODIA,
         PERCUSSÃO
     };
 
-    }
-
-
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-           String[]itensMenuprincipal = Enum.GetNames(typeof(FormacaoEnum));
-           String[]itensMenuCatergoria = Enum.GetNames(typeof(CategoriaEnum));
+            //TODO: Verificar com o professor o inicio do menu de opções
+            string[] itensMenuPrincipal = Enum.GetNames(typeof(FormacaoEnum));
+            string[] itensMenuCategoria = Enum.GetNames(typeof(CategoriaEnum));
+
+            var opcoesFormacao = new List<string>()
+            {
+                "   - 0                     ",
+                "   - 1                 "   
+            };
+
+            opcoesFormacao = 0;
+
+            int opcaoFormacaoSelecionada; 
+            string menuBar = "=============================================";
+
+            do{
+                do
+            {
+                bool formacaoEscolhida = false;
+                Console.Clear();
+                System.Console.WriteLine(menuBar);
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                System.Console.WriteLine("     Seja bem-vind@!    ");
+                System.Console.WriteLine("     Escolha uma formação:    ");
+                Console.ResetColor();
+                Console.WriteLine(menuBar);
+,,
+                for(int i = 0; i< opcoesFormacao.Count; i++)
+                {
+                    string titulo = itensMenuPrincipal[i];
+                    if(opcaoFormacaoSelecionada == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(opcoesFormacao[opcaoFormacaoSelecionada].Replace("-", ">").Replace(i.ToString(),titulo));
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine(opcoesFormacao[i].Replace(i.ToString(), titulo));
+                    }
+                }
+                
+                var key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                    opcaoFormacaoSelecionada = opcaoFormacaoSelecionada == 0 ? opcaoFormacaoSelecionada : --opcaoFormacaoSelecionada;
+                    break;
+                    case ConsoleKey.DownArrow:
+                    opcaoFormacaoSelecionada = opcaoFormacaoSelecionada == opcoesFormacao.Count - 1 ? opcaoFormacaoSelecionada: ++opcaoFormacaoSelecionada;
+                    break;
+                    case ConsoleKey.Enter:
+                    formacaoEscolhida = true;
+                    break;
+                }
+                //TODO: Terminar menu de opções
+            }
+
+                }
+            }       
         }
     }
-}
+
