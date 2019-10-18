c6d67d6b366bfb1d67973abad8fa531dabee24f2
Renan Salvino <renansalvinomartins454@gmail.com>
2019-10-18 17:17:24 -0300

    Estuda seu maldito

---
+++ Exercícios erro/ByteBank2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ByteBank2.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                
+                ContaCorrente contaCorrente1 = new ContaCorrente(1, 1, "Alexandre");
+                ContaCorrente contaEspecial1 = new ContaCorrente  (1, 2, "Renan");
+            }
+    }
+}
+            
+        
+    
+
