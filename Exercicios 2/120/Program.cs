0b5218151099df2fd709fb6ead34d9bcb2d5df63
renansalvino <renansalvinomartins454@gmail.com>
2019-10-31 15:07:58 -0300

    31/10

---
+++ Exercicios 2/120/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+           for( i = 120; i <= 300; i++){
+
+           System.Console.WriteLine((i) );
+           }
+
+        }
+    }
+}
